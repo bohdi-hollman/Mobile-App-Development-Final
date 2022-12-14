b2340ec8093d801c0fde93522164d4afeba84456
Bohdi Hollman <bohdi.hollman@gmail.com>
2022-12-13 19:38:37 -0700

    Initial commit

---
+++ app/src/main/java/com/sum/room/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sum.room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sum.room.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
